b6fec30d105088e507788e4769770ae7ada25630
alexander-turner <turneral1@grinnell.edu>
2013-09-22 13:48:46 -0500

    Expanded the Calculator class. Created some tests for the Calculator class.

--- src/turneral1/grinnell/edu/hw4/Calculator.java
+++ src/turneral1/grinnell/edu/hw4/Calculator.java
@@ -7,100 +7,129 @@ import java.math.BigInteger;
 
 public class Calculator {
     // initialize storage elements
-    static private int storageSize = 8;
-    static private double r[] = new double[storageSize];
+    private int storageSize;
+    private double r[] = new double[storageSize];
     
+    public Calculator() throws Exception {
+	// minimum storage size
+	this.storageSize = 8;
+	this.r = new double[this.storageSize];
+    } // Calculator()
+    
+    public Calculator(int size) throws Exception {
+	this.storageSize = size;
+	this.r = new double[this.storageSize];
+    } // Calculator(int storageSize)
+
     // Much of the code is taken from the Calculator class for Alex Turner and
     //  Daniel Goldstein's csc-207/hw3 repository
-    static public double evaluate(String expression) throws Exception {
-    int storageIndex = -1;
-    double value = 0;
-    
-    if ((expression.substring(0,5)).matches("[r]\\d = ")) {
-        // store the specified index
-        storageIndex = expression.charAt(1) - 48;
-        // truncate the assignment from the expression
-        expression = expression.substring(expression.indexOf(2, ' ')+1);
-    } // if
+    public double evaluate(String expression) throws Exception {
+	int storageIndex = -1;
+	double value = 0;
+	
+	if (expression.length() >= 5) {
+	    if ((expression.substring(0,5)).matches("[r]\\d = ")) {
+		// store the specified index
+		storageIndex = expression.charAt(1) - 48;
+		// truncate the assignment from the expression
+		expression = expression.substring(expression.indexOf(' ',expression.indexOf(' ')+1)+1);
+	    } // if
+	} // if
+	
+	char symbol = '+';
+	for (int j = 0; j<expression.indexOf(' '); j++) {
+	    value += (expression.charAt(j)-48)*Math.pow(10,expression.indexOf(' ')-j-1);
+	} // for
+	
+	// store the next symbol
+	symbol = expression.charAt(expression.indexOf(' ')+1);
+	// moves to the next number
+	expression = expression.substring(expression.indexOf(' ')+3);
+	System.out.println(value + " " + expression);
+	while(expression.length() > 0) {
+	    double num = 0;
+	    int len = 0;
 
-    char symbol = '+';
+	    if (expression.indexOf(' ') != -1) {
+		len = expression.indexOf(' ');
+	    } // if
+	    else {
+		len = expression.length();
+	    } // else
+	    
+	    String expr = expression.substring(0,len);
+	    
+	    if (expr.matches("[r]\\d")) { // stored value
+		// check that r[num] has a value
+		num = r[expr.charAt(expr.indexOf('r')+1)-48];
+	    } // if
 
-    while(expression.length() > 0) {
-        double num = 0;
-        int len = 0;
+	    else if (expr.matches("\\d/\\d")) { // fraction
+		int j = 0;
+		BigInteger numerator = BigInteger.valueOf(0);
+		
+		for (; expression.charAt(j) != '/'; j++) {
+		    numerator = numerator.add(BigInteger.valueOf((expression.charAt(j)-48)*((long)Math.pow(10,len-j-1))));
+		} // for
+		
+		BigInteger denominator = BigInteger.valueOf(0);
+		for (j++; j<len; j++) {
+		    denominator = denominator.add(BigInteger.valueOf((expression.charAt(j)-48)*((long)Math.pow(10,len-j-1))));
+		} // for
+		
+		num = (new Fraction(numerator,denominator)).decimalValue();
+	    } // else if
 
-        if (expression.indexOf(' ') != -1) {
-        len = expression.indexOf(' ');
-        } // if
-        else
-        len = expression.length();
-        String expr = expression.substring(0,len);
-        
-        if (expr.matches(" [r]\\d ")) { // stored value
-        // check that r[num] has a value
-        num = r[expr.charAt(expr.indexOf('r')+1)-48];
-        } // if
-        
-        else if (expr.matches(" \\d/\\d ")) { // fraction
-        int j = 0;
-        BigInteger numerator = BigInteger.valueOf(0);
-        for (; j<len && expression.charAt(j) != '/'; j++)
-            numerator.add(BigInteger.valueOf((expression.charAt(j)-48)*((long)Math.pow(10,len-j-1))));
-        BigInteger denominator = BigInteger.valueOf(0);
-        for (; j<len && expression.charAt(j) != ' '; j++)
-            numerator.add(BigInteger.valueOf((expression.charAt(j)-48)*((long)Math.pow(10,len-j-1))));
-        num = (new Fraction(numerator,denominator)).decimalValue();
-        } // else if
-        
-        else { // for normal integers
-        for (int j = 0; j<len && expression.charAt(j) != ' '; j++)
-            num += (expression.charAt(j)-48)*Math.pow(10,len-j-1);
-        } // else
+	    else { // for normal integers
+		for (int j = 0; j<len; j++) {
+		    num += (expression.charAt(j)-48)*Math.pow(10,len-j-1);
+		} // for
+	    } // else
 
-        switch (symbol) {
-        case '+':
-        value += num;
-        break;
-        case '-':
-        value -= num;
-        break;
-        case '*':
-        value *= num;
-        break;
-        case '/':
-        if (num != 0) {
-            value /= num;
-        } // if
-        break;
-        case '^':
-        value = Math.pow(value,num);
-        break;
-        } // switch
+	    switch (symbol) {
+	    case '+':
+		value += num;
+		break;
+	    case '-':
+		value -= num;
+		break;
+	    case '*':
+		value *= num;
+		break;
+	    case '/':
+		if (num != 0) {
+		    value /= num;
+		} // if
+		break;
+	    case '^':
+		value = Math.pow(value,num);
+		break;
+	    } // switch
 
-        if (expression.indexOf(' ') == -1)
-        break;
+	    if (expression.indexOf(' ') == -1)
+		break;
 
-        // store the next symbol
-        symbol = expression.charAt(expr.indexOf(' ')+1);
-        // moves to the next number
-        expression = expression.substring(expr.indexOf(' ')+3);
-    } // while
+	    // store the next symbol
+	    symbol = expression.charAt(expr.indexOf(' ')+1);
+	    // moves to the next number
+	    expression = expression.substring(expr.indexOf(' ')+3);
+	} // while
 
-    if (storageIndex > -1 && storageIndex < storageSize) {
-        // stores value
-        r[storageIndex] = value;
-    } // if
-    
-    return value;
+	if (storageIndex > -1 && storageIndex < storageSize) {
+	    // stores value
+	    r[storageIndex] = value;
+	} // if
+
+	return value;
     } // evaluate(String expression)
-    
+
     public double[] evaluate(String[] expressions) throws Exception {
-    double values[] = new double[expressions.length];
-    
-    for(int i=0; i<expressions.length;i++) {
-        values[i] = evaluate(expressions[i]);
-    } // for
-    
-    return values;
+	double values[] = new double[expressions.length];
+
+	for(int i=0; i<expressions.length;i++) {
+	    values[i] = evaluate(expressions[i]);
+	} // for
+
+	return values;
     } // evaluate(String[] expressions)
 } // Calculator
--- src/turneral1/grinnell/edu/hw4/CalculatorTest.java
+++ src/turneral1/grinnell/edu/hw4/CalculatorTest.java
@@ -7,8 +7,18 @@ import org.junit.Test;
 public class CalculatorTest {
 
     @Test
-    public void test() {
-	fail("Not yet implemented");
-    }
+    public void test() throws Exception {
+	Calculator ti84 = new Calculator(8);
+	
+	assertEquals("Positive addition", 7, ti84.evaluate("2 + 5"), .01);
+	assertEquals("Positive subtraction", 3, ti84.evaluate("5 - 2"), .01);
+	assertEquals("Positive multiplication", 10, ti84.evaluate("2 * 5"), .01);
+	assertEquals("Positive division", 2, ti84.evaluate("10 / 5"), .01);
+	
+	assertEquals("Storage", 500, ti84.evaluate("r0 = 5 * 100"), .01);
+	assertEquals("Retrieval", 500, ti84.evaluate("r0"), .01);
+	assertEquals("Overwriting", 200, ti84.evaluate("r0 = 2 * 100"), .01);
+	
+    } // test()
 
-}
+} // CalculatorTest
