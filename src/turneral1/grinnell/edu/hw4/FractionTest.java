2958dc8a4c23b78d587293d3d0c6434d5e243155
John Brady <bradyjoh@grinnell.edu>
2013-09-23 17:25:10 -0500

    Fixed the inits of test cases, with advice from Sam. Started changing compare values to BigIntegers.

--- src/turneral1/grinnell/edu/hw4/FractionTest.java
+++ src/turneral1/grinnell/edu/hw4/FractionTest.java
@@ -2,91 +2,140 @@ package turneral1.grinnell.edu.hw4;
 
 import static org.junit.Assert.*;
 
+import java.math.BigInteger;
+
 import org.junit.Test;
 
 public class FractionTest {
 
-    Fraction exampleOne = new Fraction (1, 1);
-    Fraction exampleTwo = new Fraction (7, 23);
-    Fraction exampleThree = new Fraction (3, 2);
-    Fraction exampleFour = new Fraction (-1, 4);
+    Fraction exampleOne;
+    Fraction exampleTwo;
+    Fraction exampleThree; 
+    Fraction exampleFour;
+
+    public void initExamples() throws Exception{
+	exampleOne = new Fraction(1, 1);
+	exampleTwo = new Fraction(7, 23);
+	exampleThree = new Fraction(3, 2);
+	exampleFour = new Fraction(-1, 4);
+    } // initExamples
 
     @Test
-    public void testGetNumerator() {
-    assertEquals("example one numerator", 1, exampleOne.getNumerator());
-    assertEquals("example two numerator", 7, exampleTwo.getNumerator());
-    assertEquals("example three numerator", 3, exampleThree.getNumerator());
-    assertEquals("example four numerator", -1, exampleFour.getNumerator());
+    public void testGetNumerator() throws Exception{
+	initExamples();
+	assertEquals("example one numerator", BigInteger.valueOf(1), exampleOne.getNumerator());
+	assertEquals("example two numerator", BigInteger.valueOf(7), exampleTwo.getNumerator());
+	assertEquals("example three numerator", BigInteger.valueOf(3), exampleThree.getNumerator());
+	assertEquals("example four numerator", BigInteger.valueOf(-1), exampleFour.getNumerator());
     } // testGetNumerator
 
     @Test
-    public void testGetDenominator() {
-    assertEquals("example one denominator", 1, exampleOne.getDenominator());
-    assertEquals("example two denominator", 23, exampleTwo.getDenominator());
-    assertEquals("example three denominator", 2, exampleThree.getDenominator());
-    assertEquals("example four denominator", 4, exampleFour.getDenominator());
+    public void testGetDenominator() throws Exception{
+	initExamples();
+	assertEquals("example one denominator", BigInteger.valueOf(1), exampleOne.getDenominator());
+	assertEquals("example two denominator", BigInteger.valueOf(23), exampleTwo.getDenominator());
+	assertEquals("example three denominator", BigInteger.valueOf(2),
+		exampleThree.getDenominator());
+	assertEquals("example four denominator", BigInteger.valueOf(4),
+		exampleFour.getDenominator());
     } // testGetDenominator
 
     @Test
     public void testMultiply() throws Exception {
-    Fraction exampleOneMultiplied = new Fraction(exampleOne.getNumerator(), exampleOne.getDenominator());
-    exampleOneMultiplied.multiply(exampleOneMultiplied, exampleFour);
-    Fraction exampleTwoMultiplied = new Fraction(exampleTwo.getNumerator(), exampleTwo.getDenominator());
-    exampleTwoMultiplied.multiply(exampleTwoMultiplied, exampleThree);
-    assertEquals("example one multiply numerator", -1, exampleOneMultiplied.getNumerator());
-    assertEquals("example one multiply denominator", 4, exampleOneMultiplied.getDenominator());
-    assertEquals("example two multiply numerator", 21, exampleTwoMultiplied.getNumerator());
-    assertEquals("example two multiply denominator", 46, exampleTwoMultiplied.getDenominator());
+	initExamples();
+	Fraction exampleOneMultiplied = new Fraction(exampleOne.getNumerator(),
+		exampleOne.getDenominator());
+	exampleOneMultiplied.multiply(exampleOneMultiplied, exampleFour);
+	Fraction exampleTwoMultiplied = new Fraction(exampleTwo.getNumerator(),
+		exampleTwo.getDenominator());
+	exampleTwoMultiplied.multiply(exampleTwoMultiplied, exampleThree);
+	assertEquals("example one multiply numerator", BigInteger.valueOf(-1),
+		exampleOneMultiplied.getNumerator());
+	assertEquals("example one multiply denominator", BigInteger.valueOf(4),
+		exampleOneMultiplied.getDenominator());
+	assertEquals("example two multiply numerator", BigInteger.valueOf(21),
+		exampleTwoMultiplied.getNumerator());
+	assertEquals("example two multiply denominator", BigInteger.valueOf(46),
+		exampleTwoMultiplied.getDenominator());
     } // testMultiply
 
     @Test
     public void testDivide() throws Exception {
-    Fraction exampleOneDivided = new Fraction(exampleOne.getNumerator(), exampleOne.getDenominator());
-    exampleOneDivided.divide(exampleOneDivided, exampleFour);
-    Fraction exampleTwoDivided = new Fraction(exampleTwo.getNumerator(), exampleTwo.getDenominator());
-    Fraction.divide(exampleTwoDivided, exampleThree);
-    assertEquals("example one divide numerator", 4, exampleOneDivided.getNumerator());
-    assertEquals("example one divide denominator", -1, exampleOneDivided.getDenominator());
-    assertEquals("example two divide numerator", 14, exampleTwoDivided.getNumerator());
-    assertEquals("example two divide denominator", 69, exampleTwoDivided.getDenominator());
+	initExamples();
+	Fraction exampleOneDivided = new Fraction(exampleOne.getNumerator(),
+		exampleOne.getDenominator());
+	exampleOneDivided.divide(exampleOneDivided, exampleFour);
+	Fraction exampleTwoDivided = new Fraction(exampleTwo.getNumerator(),
+		exampleTwo.getDenominator());
+	Fraction.divide(exampleTwoDivided, exampleThree);
+	assertEquals("example one divide numerator", BigInteger.valueOf(4),
+		exampleOneDivided.getNumerator());
+	assertEquals("example one divide denominator", BigInteger.valueOf(-1),
+		exampleOneDivided.getDenominator());
+	assertEquals("example two divide numerator", BigInteger.valueOf(14),
+		exampleTwoDivided.getNumerator());
+	assertEquals("example two divide denominator", BigInteger.valueOf(69),
+		exampleTwoDivided.getDenominator());
     } // testDivide
 
     @Test
-    public void testComputeValue() {    
-    assertEquals("exampleOne value", 1.0/1.0, exampleOne.decimalValue(), .1);
-    assertEquals("exampleTwo value", 7.0/23.0, exampleTwo.decimalValue(), .1);
-    assertEquals("exampleThree value", 3.0/2.0, exampleThree.decimalValue(), .1);    
-    assertEquals("exampleFour value", -1.0/4.0, exampleFour.decimalValue(), .1);
+    public void testComputeValue() throws Exception{
+	initExamples();
+	assertEquals("exampleOne value", 1.0 / 1.0, exampleOne.decimalValue(),
+		.1);
+	assertEquals("exampleTwo value", 7.0 / 23.0, exampleTwo.decimalValue(),
+		.1);
+	assertEquals("exampleThree value", 3.0 / 2.0,
+		exampleThree.decimalValue(), .1);
+	assertEquals("exampleFour value", -1.0 / 4.0,
+		exampleFour.decimalValue(), .1);
     } // testComputeValue
 
     @Test
     public void testAdd() throws Exception {
-    Fraction exampleOneAdded = new Fraction(exampleOne.getNumerator(), exampleOne.getDenominator());
-    exampleOneAdded.add(exampleOneAdded, exampleFour);
-    Fraction exampleTwoAdded = new Fraction(exampleTwo.getNumerator(), exampleTwo.getDenominator());
-    exampleTwoAdded.add(exampleTwoAdded, exampleThree);
-    assertEquals("example one add numerator", 3, exampleOneAdded.getNumerator());
-    assertEquals("example one add denominator", 4, exampleOneAdded.getDenominator());
-    assertEquals("example two add numerator", 83, exampleTwoAdded.getNumerator());
-    assertEquals("example two add denominator", 46, exampleTwoAdded.getDenominator());
+	initExamples();
+	Fraction exampleOneAdded = new Fraction(exampleOne.getNumerator(),
+		exampleOne.getDenominator());
+	exampleOneAdded.add(exampleOneAdded, exampleFour);
+	Fraction exampleTwoAdded = new Fraction(exampleTwo.getNumerator(),
+		exampleTwo.getDenominator());
+	exampleTwoAdded.add(exampleTwoAdded, exampleThree);
+	assertEquals("example one add numerator", BigInteger.valueOf(1), 
+		exampleOneAdded.getNumerator());
+	assertEquals("example one add denominator", BigInteger.valueOf(4),
+		exampleOneAdded.getDenominator());
+	assertEquals("example two add numerator", BigInteger.valueOf(83),
+		exampleTwoAdded.getNumerator());
+	assertEquals("example two add denominator", BigInteger.valueOf(46),
+		exampleTwoAdded.getDenominator());
     } // testAdd
 
     @Test
     public void testSubtract() throws Exception {
-    Fraction exampleOneSubtracted = new Fraction(exampleOne.getNumerator(), exampleOne.getDenominator());
-    exampleOneSubtracted.subtract(exampleOneSubtracted, exampleFour);
-    Fraction exampleTwoSubtracted = new Fraction(exampleTwo.getNumerator(), exampleTwo.getDenominator());
-    exampleTwoSubtracted.subtract(exampleTwoSubtracted, exampleThree);
-    assertEquals("example one subtract numerator", 5, exampleOneSubtracted.getNumerator());
-    assertEquals("example one subtract denominator", 4, exampleOneSubtracted.getDenominator());
-    assertEquals("example two subtract numerator", -55, exampleTwoSubtracted.getNumerator());
-    assertEquals("example two subtract denominator", 46, exampleTwoSubtracted.getDenominator());
+	initExamples();
+	Fraction exampleOneSubtracted = new Fraction(exampleOne.getNumerator(),
+		exampleOne.getDenominator());
+	exampleOneSubtracted.subtract(exampleOneSubtracted, exampleFour);
+	Fraction exampleTwoSubtracted = new Fraction(exampleTwo.getNumerator(),
+		exampleTwo.getDenominator());
+	exampleTwoSubtracted.subtract(exampleTwoSubtracted, exampleThree);
+	assertEquals("example one subtract numerator", BigInteger.valueOf(5),
+		exampleOneSubtracted.getNumerator());
+	assertEquals("example one subtract denominator", BigInteger.valueOf(4),
+		exampleOneSubtracted.getDenominator());
+	assertEquals("example two subtract numerator", BigInteger.valueOf(-55),
+		exampleTwoSubtracted.getNumerator());
+	assertEquals("example two subtract denominator", BigInteger.valueOf(46),
+		exampleTwoSubtracted.getDenominator());
     } // testSubtract
-    
-    @Test
-    public void testSimplify() throws Exception{
-	Fraction exampleOne = new Fraction(1,1);
+
+   /*@Test
+    //we can't test this, as it is a private function. Change to public for testing, then revert? 
+    public void testSimplify() throws Exception {
+	initExamples();
+	Fraction exampleOne = new Fraction(1, 1);
 	exampleOne.simplify();
-    assertEquals("1/1 simplified", new Fraction(1,1), exampleOne);
+	assertEquals("1/1 simplified", new Fraction(1, 1), exampleOne);
     } // testSimplify
+    */
 } // FractionTest
