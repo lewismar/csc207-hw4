fd9af1e67adc4761fede863674e7e7b677254d20
alexander-turner <turneral1@grinnell.edu>
2013-09-23 20:51:17 -0500

    Added more functionality to Calculator.java

--- src/turneral1/grinnell/edu/hw4/Calculator.java
+++ src/turneral1/grinnell/edu/hw4/Calculator.java
@@ -9,13 +9,13 @@ public class Calculator {
     // initialize storage elements
     private int storageSize;
     private double r[] = new double[storageSize];
-    
+
     public Calculator() throws Exception {
-	// minimum storage size
+	// minimum storage size is 8
 	this.storageSize = 8;
 	this.r = new double[this.storageSize];
     } // Calculator()
-    
+
     public Calculator(int size) throws Exception {
 	this.storageSize = size;
 	this.r = new double[this.storageSize];
@@ -27,14 +27,15 @@ public class Calculator {
 	int storageIndex = -1;
 	double value = 0;
 	char symbol = '+';
-	
-	if ((expression.substring(0,1).matches("[r]\\d"))) {
+
+	// expression begins with a memory call
+	if ((expression.substring(0,2).matches("[r]\\d"))) {
 	    if (expression.length() >= 5 && (expression.substring(0,5)).matches("[r]\\d = ")) {
-		    // store the specified index
-		    storageIndex = expression.charAt(1) - 48;
-		    // truncate the assignment from the expression
-		    expression = expression.substring(expression.indexOf(' ',expression.indexOf(' ')+1)+1);
-		} // if
+		// store the specified index
+		storageIndex = expression.charAt(1) - 48;
+		// truncate the assignment from the expression
+		expression = expression.substring(expression.indexOf(' ',expression.indexOf(' ')+1)+1);
+	    } // if
 	    else  {
 		value = r[expression.charAt(1) - 48];
 		// store the next symbol
@@ -43,47 +44,68 @@ public class Calculator {
 		expression = expression.substring(expression.indexOf(' ')+3);
 	    } // else
 	} // if
-	
+
+	//read expression
 	while(expression.length() > 0) {
+	    boolean isNegative = false;
 	    double num = 0;
 	    int len = 0;
 
+	    // the number is negative
+	    if(expression.charAt(0) == '-') {
+		isNegative = !isNegative;
+		expression = expression.substring(1);
+	    } // else
 	    if (expression.indexOf(' ') != -1) {
 		len = expression.indexOf(' ');
 	    } // if
 	    else {
 		len = expression.length();
 	    } // else
-	    
+
 	    String expr = expression.substring(0,len);
-	    
+
 	    if (expr.matches("[r]\\d")) { // stored value
-		// check that r[num] has a value
+		// check that r[d] has a value
 		num = r[expr.charAt(expr.indexOf('r')+1)-48];
 	    } // if
 
-	    else if (expr.matches("\\d/\\d")) { // fraction
+	    // fractional input
+	    else if (expr.matches("\\d/\\d") || expr.matches("\\d/\\-\\d")) { 
 		int j = 0;
 		BigInteger numerator = BigInteger.valueOf(0);
-		
+
+		// read numerator
 		for (; expression.charAt(j) != '/'; j++) {
 		    numerator = numerator.add(BigInteger.valueOf((expression.charAt(j)-48)*((long)Math.pow(10,expression.indexOf('/')-j-1))));
-		} // for
-		
+		} // for		
+
+		// the denominator is negative
+		if(expression.charAt(j+1) == '-') {
+		    isNegative = !isNegative;
+		    j++;
+		} // if
+
+		// read denominator
 		BigInteger denominator = BigInteger.valueOf(0);
 		for (j++; j<len; j++) {
 		    denominator = denominator.add(BigInteger.valueOf((expression.charAt(j)-48)*((long)Math.pow(10,len-j-1))));
 		} // for
-		
+
 		num = (new Fraction(numerator,denominator)).decimalValue();
 	    } // else if
-
-	    else { // for normal integers
+	    
+	    // for normal integers
+	    else { 
 		for (int j = 0; j<len; j++) {
 		    num += (expression.charAt(j)-48)*Math.pow(10,len-j-1);
 		} // for
 	    } // else
-
+	    
+	    if (isNegative) {
+		num *= -1;
+	    } // if
+	    
 	    switch (symbol) {
 	    case '+':
 		value += num;
@@ -104,9 +126,10 @@ public class Calculator {
 		break;
 	    } // switch
 
-	    if (expression.indexOf(' ') == -1)
+	    if (expression.indexOf(' ') == -1) {
 		break;
-
+	    } // if
+	    
 	    // store the next symbol
 	    symbol = expression.charAt(expression.indexOf(' ')+1);
 	    // moves to the next number
@@ -124,7 +147,7 @@ public class Calculator {
     public double[] evaluate(String[] expressions) throws Exception {
 	double values[] = new double[expressions.length];
 
-	for(int i=0; i<expressions.length;i++) {
+	for(int i=0; i<expressions.length; i++) {
 	    values[i] = evaluate(expressions[i]);
 	} // for
 
--- src/turneral1/grinnell/edu/hw4/CalculatorTest.java
+++ src/turneral1/grinnell/edu/hw4/CalculatorTest.java
@@ -2,6 +2,7 @@ package turneral1.grinnell.edu.hw4;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CalculatorTest {
@@ -10,17 +11,36 @@ public class CalculatorTest {
     public void test() throws Exception {
 	Calculator ti84 = new Calculator(8);
 	
+	// Normal operations
 	assertEquals("Positive addition", 7, ti84.evaluate("2 + 5"), .01);
 	assertEquals("Positive subtraction", 3, ti84.evaluate("5 - 2"), .01);
 	assertEquals("Positive multiplication", 10, ti84.evaluate("2 * 5"), .01);
 	assertEquals("Positive division", 2, ti84.evaluate("10 / 5"), .01);
+	assertEquals("Negative addition", 3, ti84.evaluate("-2 + 5"), .01);
+	assertEquals("Negative subtraction", -7, ti84.evaluate("-5 - 2"), .01);
+	assertEquals("Negative multiplication", -10, ti84.evaluate("2 * -5"), .01);
+	assertEquals("Negative division", -2, ti84.evaluate("-10 / 5"), .01);
 	
+	// Fractions
 	assertEquals("Fractional input", 1, ti84.evaluate("1/1"), .01);
+	assertEquals("Fractional input", .2, ti84.evaluate("1/5"), .01);
+	assertEquals("Fractional input", 5, ti84.evaluate("5/1"), .01);
+	assertEquals("Double negative fraction", 1, ti84.evaluate("-1/-1"), .01);
+	assertEquals("Double negative fraction", .2, ti84.evaluate("-1/-5"), .01);
+	assertEquals("Double negative fraction", 5, ti84.evaluate("-5/-1"), .01);
+	assertEquals("Negative denominator", -1, ti84.evaluate("1/-1"), .01);
+	assertEquals("Negative denominator", -.2, ti84.evaluate("1/-5"), .01);
+	assertEquals("Negative denominator", -5, ti84.evaluate("5/-1"), .01);
+	assertEquals("Negative numerator", -1, ti84.evaluate("-1/1"), .01);
+	assertEquals("Negative numerator", -.2, ti84.evaluate("-1/5"), .01);
+	assertEquals("Negative numerator", -5, ti84.evaluate("-5/1"), .01);
 	
+	// Memory operations
 	assertEquals("Storage", 500, ti84.evaluate("r0 = 5 * 100"), .01);
 	assertEquals("Retrieval", 500, ti84.evaluate("r0"), .01);
 	assertEquals("Overwriting", 200, ti84.evaluate("r0 = 2 * 100"), .01);
 	
+	// Exceptions
+	assertEquals("Divide by 0", "fail", ti84.evaluate("1/0"));
     } // test()
-
 } // CalculatorTest
